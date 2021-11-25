406934e38716c55a00d194986d579c690e47a7ce
Nikolla2502 <nikolla@wanadoo.fr>
2021-11-25 09:03:13 +0100

    creation des routes sauce dans app.js

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 
-const app = express();
+const sauce = require('./models/sauce');
+
+
 
 // connexion au fichier d'environnement
 require('dotenv').config();
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
 
 // connexion MongoDB
 mongoose.connect(process.env.MONGO_URI,
@@ -22,7 +18,38 @@ mongoose.connect(process.env.MONGO_URI,
 
 
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});    
 
+app.post('/api/sauce',(req,res,next) => {
+    delete req.body._id;        // on enleve le champs id genere par mongodb du corps de la requete
+    const sauce = new sauce({
+        ...req.body           // ...req.body premet de recuperer l'integralite du corps de la requete
+    })
+    sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
+    .catch(error => res.status(400).json({ error}));
+});
+app.use('/api/sauce',(req,res,next) => {
+    sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error =>res.status(400).json({ error}));
+});
+app.get('/api/sauce/:id', (req, res, next) => {
+    sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+    });
+
+app.put('/api/sauce/:id', (req, res, next) => {
+    sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+    });
 
 
 module.exports = app;
